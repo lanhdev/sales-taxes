5a9e4357905f4a64f9efcd872a271f6979f6f1d5
Lanh Hoang <lanh.hoangcong@gmail.com>
2019-01-13 02:03:00 +0700

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,9 +1,6 @@
-require './app/receipt_printer.rb'
-require './lib/csv_importer.rb'
-require './lib/csv_exporter.rb'
-require './lib/tax_rate.rb'
-require './app/decorators/format_number.rb'
-require './app/models/item.rb'
-require './app/models/receipt.rb'
+directories = %w(app lib)
+directories.each do |directory|
+  Dir.glob("./#{directory}/**/*.rb").each do { |file| require file }
+end
 
 ReceiptPrinter.new.execute
