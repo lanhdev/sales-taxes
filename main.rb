394b66bf21d226a63946925d7a9514a2e853be57
Lanh Hoang <lanh.hoangcong@gmail.com>
2019-01-13 02:56:19 +0700

    Update README

--- main.rb
+++ main.rb
@@ -3,4 +3,5 @@ directories.each do |directory|
   Dir.glob("./#{directory}/**/*.rb").each { |file| require file }
 end
 
-ReceiptPrinter.new(Dir.pwd + '/resources/input1.csv').execute
+input_file = ARGV[0]
+ReceiptPrinter.new(input_file).execute
